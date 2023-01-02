f7617643e7f10b9635a98d62450a6606ff7497da
Owais Tahir <oo_wais@hotmail.com>
2023-01-02 12:27:24 +0500

    Update 3_Cancer Detection.py

--- pages/3_Cancer Detection.py
+++ pages/3_Cancer Detection.py
@@ -1,6 +1,26 @@
 import streamlit as st
+from streamlit_lottie import st_lottie
+import json
 
-st.set_page_config(page_title='Cancer Detection')
+#title
+st.set_page_config(page_title='Cancer Detection', layout='wide')
+
+#add lottie animation
+def load_lottiefile(filepath: str):
+    with open(filepath, 'r') as f:
+        return json.load(f)
+
+lottie_file = load_lottiefile('96262-detective-search.json')
+col1, col2, col3= st.columns(3)
+with col1:
+    st_lottie(
+    lottie_file,
+    reverse=False,
+    loop=True,
+    quality='low',
+    height=100,
+    width=200
+)
 
 st.title('This is the Cancer Detection page')
 
