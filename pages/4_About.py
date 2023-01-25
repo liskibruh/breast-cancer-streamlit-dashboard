12397a2e928fcf7fd90cc138659a2cdb8efe53e6
Owais Tahir <oo_wais@hotmail.com>
2023-01-25 21:36:04 +0500

    Update 4_About.py

--- pages/4_About.py
+++ pages/4_About.py
@@ -6,3 +6,5 @@ st.title('This is the About page')
 #apply css from style.css
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+########################################################################################
